b2eff282a651a4d850589ccec229141e9c6695e2
markeymagic <markeyiscool@gmail.com>
2020-12-19 19:39:08 +1100

    cachedToken

--- utilities/Token.go
+++ utilities/Token.go
@@ -3,10 +3,19 @@ package utilities
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/acaciamoney/basiq-sdk/errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
 )
+
+type CachedToken struct {
+	Token  string
+	Expiry int
+}
 
 type Token struct {
 	Value     string
@@ -21,6 +30,16 @@ type AuthorizationResponse struct {
 }
 
 func GetToken(apiKey, apiVersion string) (*Token, *errors.APIError) {
+
+	token := GetCachedToken()
+	if token.Expiry > int(time.Now().Unix()) {
+		return &Token{
+			Value:     token.Token,
+			Validity:  time.Duration(token.Expiry) * time.Second,
+			Refreshed: time.Now(),
+		}, nil
+	}
+
 	body, _, err := NewAPI("https://au-api.basiq.io/").SetHeader("Authorization", "Basic "+apiKey).
 		SetHeader("basiq-version", apiVersion).
 		SetHeader("content-type", "application/json").
@@ -34,10 +53,59 @@ func GetToken(apiKey, apiVersion string) (*Token, *errors.APIError) {
 		fmt.Println(string(body))
 		return nil, &errors.APIError{Message: err.Error()}
 	}
+	expiry := time.Duration(data.ExpiresIn) * time.Second
+
+	SetCachedToken(CachedToken{
+		Token:  data.AccessToken,
+		Expiry: int(expiry.Seconds()),
+	})
 
 	return &Token{
 		Value:     data.AccessToken,
 		Validity:  time.Duration(data.ExpiresIn) * time.Second,
 		Refreshed: time.Now(),
 	}, nil
+}
+
+func GetCachedToken() CachedToken {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	payload, err := secretsmanager.New(sess).GetSecretValue(
+		&secretsmanager.GetSecretValueInput{
+			SecretId: aws.String("BasiqToken"),
+		},
+	)
+	if err != nil {
+		log.Print("Unable to fetch cached basiq token")
+		log.Fatal(err)
+	}
+	var token CachedToken
+	err = json.Unmarshal([]byte(*payload.SecretString), &token)
+	if err != nil {
+		log.Print("Unable to parse cached basiq token")
+		log.Fatal(err)
+	}
+	return token
+}
+
+func SetCachedToken(t CachedToken) {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	str, err := json.Marshal(t)
+	if err != nil {
+		log.Print("Unable to serialise cached basiq token")
+		log.Fatal(err)
+	}
+	_, err = secretsmanager.New(sess).PutSecretValue(
+		&secretsmanager.PutSecretValueInput{
+			SecretId:     aws.String("BasiqToken"),
+			SecretString: aws.String(string(str)),
+		},
+	)
+	if err != nil {
+		log.Print("Unable Save cached basiq token...")
+		log.Fatal(err)
+	}
 }
